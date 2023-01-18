0da0c6a1b4dc44878d23588d1a39bf99d015568c
sergey <Sergey-Bernikov@mail.ru>
2023-01-18 11:15:47 +0300

    createSpringMVCJavaCode

---
+++ src/main/java/ru/bsa/controllers/MainControllers.java
@@ -0,0 +1,11 @@
+package ru.bsa.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MainControllers {
+        @GetMapping("/")
+        public String hello() {
+            return "helloSpringMVC";
+        }
+}
