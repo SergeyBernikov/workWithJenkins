100148892909454c10dac708fbc865d4f9837b48
sergey <Sergey-Bernikov@mail.ru>
2023-01-19 01:56:21 +0300

    Option 2

--- src/main/java/ru/bsa/controllers/MainControllers.java
+++ src/main/java/ru/bsa/controllers/MainControllers.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainControllers {
 
+
         @GetMapping("/")
         public String hello() {
             return "helloSpringMVC";
         }
+
+
 }
